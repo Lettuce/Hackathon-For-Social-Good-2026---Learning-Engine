bf4242b41b1ee5a9f4ecac2ba5e16eceab2b8f73
backfireIBGM <thebeast4028@gmail.com>
2026-04-11 15:16:09 -0700

    fect: subject is name pased by getSubjectInfo.js

--- frontend/getSubjectInfo.js
+++ frontend/getSubjectInfo.js
@@ -1,7 +1,24 @@
+<<<<<<< HEAD
 pageTitle = document.title;
 
 function getQuestions(pageTitle) {
   return fetch(`../backend/data/${pageTitle}/questions.json`)
+=======
+subject = "";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const subjectName = urlParams.get("name");
+
+  if (subjectName) {
+    subject = subjectName;
+  }
+});
+
+function getQuestions(subject) {
+  return fetch(`../backend/data/${subject}/questions.json`)
+>>>>>>> 12c64a1 (fect: subject is name pased by getSubjectInfo.js)
     .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -17,7 +34,11 @@ function getQuestions(pageTitle) {
     });
 }
 
+<<<<<<< HEAD
 getQuestions(pageTitle).then((questions) => {
+=======
+getQuestions(subject).then((questions) => {
+>>>>>>> 12c64a1 (fect: subject is name pased by getSubjectInfo.js)
   if (questions) {
     console.log("Fetched Subjects Data:", questions);
     const questionsElement = document.getElementById("questions");
